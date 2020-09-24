62542914dd7d0df510861f84804e67294d786b30
hasan-py <hasan29351@gmail.com>
2020-09-24 16:14:44 +0600

    Product CRUD Start

--- client/src/components1/admin/products/AddProductModal.js
+++ client/src/components1/admin/products/AddProductModal.js
@@ -9,10 +9,14 @@ const AddProductModal = (props) => {
   const alert = (msg,type)=> <div className={`bg-${type}-200 py-2 px-4 w-full`}>{msg}</div>
   
   const [fData,setFdata] = useState({
-    cName:"",
-    cDescription:"",
-    cImage:"",
-    cStatus:"Active",
+    pName:"",
+    pDescription:"",
+    pImage:"",
+    pStatus:"Active",
+    pCategory:"",
+    pQuantity:"",
+    pPrice:"",
+    pQuantity:"",
     success:false,
     error:false
   })
@@ -65,38 +69,55 @@ const AddProductModal = (props) => {
       {/* End Black Overlay */}
 
       {/* Modal Start */}
-      <div className={`${data.addProductModal ? "" : "hidden"} fixed inset-0 m-4  flex items-center z-30 justify-center`}>
-        <div className="relative bg-white w-12/12 md:w-3/6 shadow-lg flex flex-col items-center space-y-4  overflow-y-auto px-4 py-4 md:px-8">
+      <div className={`${data.addProductModal ? "" : "hidden"} fixed inset-0 flex items-center z-30 justify-center overflow-auto`}>
+        <div className="mt-32 relative bg-white w-11/12 md:w-3/6 shadow-lg flex flex-col items-center space-y-4 px-4 py-4 md:px-8">
           <div className="flex items-center justify-between w-full pt-4">
             <span className="text-left font-semibold text-2xl tracking-wider">Add Product</span>
             {/* Close Modal */}
             <span onClick={e=> dispatch({type:"addProductModal",payload:false})} className="cursor-pointer hover:bg-gray-300 py-2 px-2 rounded-lg"><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" /></svg></span>
           </div>
-          { fData.error ? alert(fData.error,"red") : ""}
-          { fData.success ? alert(fData.success,"green") : ""}
           <form className="w-full" onSubmit={e=> submitForm(e)}>
             <div className="flex flex-col space-y-1 w-full py-4">
               <label htmlFor="name">Product Name</label>
-              <input onChange={e=> setFdata({...fData,success:false,error:false,cName:e.target.value})} value={fData.cName} className="px-4 py-2 border focus:outline-none" type="text" />
+              <input value={fData.pName} onChange={e=> setFdata({...fData,error:false,success:false,pName:e.target.value})} className="px-4 py-2 border focus:outline-none" type="text" />
             </div>
             <div className="flex flex-col space-y-1 w-full">
               <label htmlFor="description">Product Description</label>
-              <textarea onChange={e=> setFdata({...fData,success:false,error:false,cDescription:e.target.value})} value={fData.cDescription} className="px-4 py-2 border focus:outline-none" name="description" id="description" cols={5} rows={5} />
+              <textarea value={fData.pDescription} onChange={e=> setFdata({...fData,error:false,success:false,pDescription:e.target.value})} className="px-4 py-2 border focus:outline-none" name="description" id="description" cols={5} rows={5} />
+            </div>
+            <div className="flex flex-col space-y-1 w-full">
+              <label htmlFor="price">Product Price</label>
+              <input value={fData.pPrice} onChange={e=> setFdata({...fData,error:false,success:false,pPrice:e.target.value})} type="number"  className="px-4 py-2 border focus:outline-none" id="price" />
             </div>
             {/* Image Field & function */}
             <div className="flex flex-col space-y-1 w-full">
               <label htmlFor="name">Product Image</label>
-              <input onChange={e=> {setFdata({...fData,success:false,error:false,cImage:e.target.files[0]});}} className="px-4 py-2 border focus:outline-none" type="file" />
+              <input value={fData.pImage} onChange={e=> setFdata({...fData,error:false,success:false,pImage:e.target.value})} multiple onChange={e=> {setFdata({...fData,success:false,error:false,cImage:e.target.files[0]});}} className="px-4 py-2 border focus:outline-none" type="file" />
             </div>
             <div className="flex flex-col space-y-1 w-full">
               <label htmlFor="status">Product Status</label>
-              <select name="status" onChange={e=> setFdata({...fData,success:false,error:false,cStatus:e.target.value})} className="px-4 py-2 border focus:outline-none" id="status">
+              <select value={fData.pStatus} onChange={e=> setFdata({...fData,error:false,success:false,pStatus:e.target.value})} name="status" onChange={e=> setFdata({...fData,success:false,error:false,cStatus:e.target.value})} className="px-4 py-2 border focus:outline-none" id="status">
+                <option name="status" value="Active">Active</option>
+                <option name="status" value="Disabled">Disabled</option>
+              </select>
+            </div>
+            <div className="flex flex-col space-y-1 w-full">
+              <label htmlFor="quantity">Product Quantitiy</label>
+              <input value={fData.pQuantity} onChange={e=> setFdata({...fData,error:false,success:false,pQuantity:e.target.value})} type="number" className="px-4 py-2 border focus:outline-none" id="quantity" />
+            </div>
+            <div className="flex flex-col space-y-1 w-full">
+              <label htmlFor="offer">Product Offfer % </label>
+              <input value={fData.pOffer} onChange={e=> setFdata({...fData,error:false,success:false,pOffer:e.target.value})} type="number"  className="px-4 py-2 border focus:outline-none" id="offer" />
+            </div>
+            <div className="flex flex-col space-y-1 w-full">
+              <label htmlFor="status">Product Category</label>
+              <select value={fData.pCategory} onChange={e=> setFdata({...fData,error:false,success:false,pCategory:e.target.value})} name="status" onChange={e=> setFdata({...fData,success:false,error:false,cStatus:e.target.value})} className="px-4 py-2 border focus:outline-none" id="status">
                 <option name="status" value="Active">Active</option>
                 <option name="status" value="Disabled">Disabled</option>
               </select>
             </div>
             <div className="flex flex-col space-y-1 w-full pb-4 md:pb-6 mt-4">
-              <button type="submit" className="bg-gray-800 text-gray-100 text-lg font-medium py-2">Create category</button>
+              <button type="submit" className="bg-gray-800 text-gray-100 text-lg font-medium py-2">Create product</button>
             </div>
           </form>
         </div>

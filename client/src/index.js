6f415f78e58673129a4528d5b79eb6c3837b1f7c
Amruth Shyju <amruthshyju@gamil.com>
2024-06-03 17:38:48 +0530

    Improving user experience on the login and signup pages using Notistack.

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
+import {SnackbarProvider} from 'notistack'
 ReactDOM.render(
   <React.StrictMode>
+<SnackbarProvider maxSnack={3}> 
     <App />
+    </SnackbarProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
